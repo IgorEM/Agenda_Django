863ba46fa6b1abf21cd017162bf5b093a5d8288e
IgorEM <iem@cin.ufpe.br>
2021-09-19 19:37:01 -0300

    criando o model/Tabela Evento, makemigrations-sqlmigrate-migrate Registrando o Modelo Criado em admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from core.models import Evento
 # Register your models here.
+#Registrando o modelo Evento criado
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ('usuario','data_evento',)
+
+admin.site.register(Evento, EventoAdmin) #essa classe registra essa classe aqui
--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+#python manage.py makemigrations core
+#python manage.py sqlmigrate core 0001
+#python manage.py migrate core 0001
 
 # Create your models here.
+class Evento(models.Model):
+    titulo = models.CharField(max_length = 100)
+    descricao = models.TextField(blank = True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete= models.CASCADE)
+
+    class Meta: #metadados da tabela, forçando o nome da tabela core_evento se tornar evento
+        db_table = 'evento'
+
+    def __str__(self): #agora o evento aparece como object 1
+        return self.titulo
